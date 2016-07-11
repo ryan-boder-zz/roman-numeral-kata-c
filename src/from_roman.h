645e8d66f47c01c3df7f6bcbd589e07d58c92a82
ryan-boder <ryan.boder@gmail.com>
2016-07-11 09:43:06 -0400

    Added comment

--- src/from_roman.h
+++ src/from_roman.h
@@ -1,6 +1,11 @@
 #ifndef FROM_ROMAN_H
 #define FROM_ROMAN_H
 
+/**
+ * Converts a roman numeral to an integer.
+ *
+ * The caller is responsible for ensuring that the input is a valid roman numeral.
+ */
 int roman_numeral_to_integer(const char* roman);
 
 #endif
