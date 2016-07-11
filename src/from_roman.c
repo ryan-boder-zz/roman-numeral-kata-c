167c5961f78d23ae0c5365244a81e9fbaf98c32a
ryan-boder <ryan.boder@gmail.com>
2016-07-11 00:47:04 -0400

    Added test for converting V

--- src/from_roman.c
+++ src/from_roman.c
@@ -1,6 +1,34 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "from_roman.h"
+
+
+int digit_to_int(char digit)
+{
+  switch (digit)
+    {
+    case 'I': return 1;
+    case 'V': return 5;
+    case 'X': return 10;
+    case 'L': return 50;
+    case 'C': return 100;
+    case 'D': return 500;
+    case 'M': return 1000;
+    default: return 0;
+    }
+}
+
+
+
 
 
 int roman_numeral_to_integer(char* roman)
 {
-  return 1;
+  int result = 0;
+  for (int i = 0; i < strlen(roman); i++)
+    {
+      result += digit_to_int(roman[i]);
+    }
+  return result;
 }
--- test/check_from_roman.c
+++ test/check_from_roman.c
@@ -12,11 +12,35 @@ START_TEST (should_return_1_when_given_I)
 END_TEST
 
 
+START_TEST (should_return_2_when_given_II)
+{
+  ck_assert_int_eq(2, roman_numeral_to_integer("II"));
+}
+END_TEST
+
+
+START_TEST (should_return_3_when_given_III)
+{
+  ck_assert_int_eq(3, roman_numeral_to_integer("III"));
+}
+END_TEST
+
+
+START_TEST (should_return_5_when_given_V)
+{
+  ck_assert_int_eq(5, roman_numeral_to_integer("V"));
+}
+END_TEST
+
+
 Suite* check_from_roman_suite(void)
 {
   Suite* suite = suite_create("Convert Roman Numerals to Integers");
   TCase* core = tcase_create("Core");
   tcase_add_test(core, should_return_1_when_given_I);
+  tcase_add_test(core, should_return_2_when_given_II);
+  tcase_add_test(core, should_return_3_when_given_III);
+  tcase_add_test(core, should_return_5_when_given_V);
   suite_add_tcase(suite, core);  
   return suite;
 }
