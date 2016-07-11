544844ae95775c082e145beef6238edd1af6634c
ryan-boder <ryan.boder@gmail.com>
2016-07-10 23:12:59 -0400

    Added test for I + I

--- src/add.c
+++ src/add.c
@@ -1,6 +1,17 @@
 #include <stdlib.h>
+#include <string.h>
+
 
 char* roman_math_add(char* a, char* b)
 {
-  return NULL;
+  if (NULL == a || NULL == b)
+    return NULL;
+  size_t a_length = strlen(a);
+  size_t b_length = strlen(b);
+  if (0 == a_length || 0 == b_length)
+    return NULL;
+  char* result = malloc(a_length + b_length + 1);
+  strcpy(result, a);
+  strcat(result, b);
+  return result;
 }
--- test/check_add.c
+++ test/check_add.c
@@ -5,7 +5,7 @@
 #include "roman_math.h"
 
 
-START_TEST (should_return_null_when_given_null_inputs)
+START_TEST (should_return_NULL_when_given_NULL_inputs)
 {
   ck_assert_ptr_eq(NULL, roman_math_add(NULL, NULL));
   ck_assert_ptr_eq(NULL, roman_math_add("", NULL));
@@ -14,7 +14,7 @@ START_TEST (should_return_null_when_given_null_inputs)
 END_TEST
 
 
-START_TEST (should_return_null_when_given_empty_inputs)
+START_TEST (should_return_NULL_when_given_empty_inputs)
 {
   ck_assert_ptr_eq(NULL, roman_math_add("", ""));
   ck_assert_ptr_eq(NULL, roman_math_add("I", ""));
@@ -23,12 +23,20 @@ START_TEST (should_return_null_when_given_empty_inputs)
 END_TEST
 
 
+START_TEST (should_return_II_when_given_I_and_I)
+{
+  ck_assert_str_eq("II", roman_math_add("I", "I"));
+}
+END_TEST
+
+
 Suite* check_add_suite(void)
 {
   Suite* suite = suite_create("Add Roman Numerals");
   TCase* core = tcase_create("Core");
-  tcase_add_test(core, should_return_null_when_given_null_inputs);
-  tcase_add_test(core, should_return_null_when_given_empty_inputs);
+  tcase_add_test(core, should_return_NULL_when_given_NULL_inputs);
+  tcase_add_test(core, should_return_NULL_when_given_empty_inputs);
+  tcase_add_test(core, should_return_II_when_given_I_and_I);
   suite_add_tcase(suite, core);  
   return suite;
 }
