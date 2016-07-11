15686035644a6cb232343fe686687fab0543ab9a
ryan-boder <ryan.boder@gmail.com>
2016-07-11 09:13:29 -0400

    Added test for convert XI

--- test/check_from_roman.c
+++ test/check_from_roman.c
@@ -103,6 +103,13 @@ START_TEST (should_return_9_when_given_IX)
 END_TEST
 
 
+START_TEST (should_return_11_when_given_XI)
+{
+  ck_assert_int_eq(11, roman_numeral_to_integer("XI"));
+}
+END_TEST
+
+
 Suite* check_from_roman_suite(void)
 {
   Suite* suite = suite_create("Convert Roman Numerals to Integers");
@@ -121,6 +128,7 @@ Suite* check_from_roman_suite(void)
   tcase_add_test(core, should_return_7_when_given_VII);
   tcase_add_test(core, should_return_8_when_given_VIII);
   tcase_add_test(core, should_return_9_when_given_IX);
+  tcase_add_test(core, should_return_11_when_given_XI);
   suite_add_tcase(suite, core);  
   return suite;
 }
