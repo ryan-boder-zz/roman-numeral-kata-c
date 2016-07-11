8a6cfeb0be239a4c8d5d96f76f28e713d24ae00b
ryan-boder <ryan.boder@gmail.com>
2016-07-11 18:48:14 -0400

    Added comment documenting integer_to_roman_numeral

--- src/to_roman.h
+++ src/to_roman.h
@@ -2,9 +2,9 @@
 #define TO_ROMAN_H
 
 /**
- * Converts an integer to a roman numeral.
+ * Returns a Roman Numeral for the given integer or NULL if integer is invalid.
  *
- * The caller is responsible for ensuring that the input is in the range of valid roman numerals.
+ * The returned string is dynamically allocated and must be freed by the caller.
  */
 const char* integer_to_roman_numeral(int integer);
 
