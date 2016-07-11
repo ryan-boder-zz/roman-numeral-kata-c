0e1944a542c89dd078fde13f8c193d5912dc7530
ryan-boder <ryan.boder@gmail.com>
2016-07-11 19:35:48 -0400

    Added comment for roman_math_add

--- src/roman_math.h
+++ src/roman_math.h
@@ -1,6 +1,13 @@
 #ifndef ROMAN_MATH_H
 #define ROMAN_MATH_H
 
+/**
+ * Adds 2 Roman Numerals.
+ *
+ * Returns the result or NULL on error.
+ *
+ * The returned string is dynamically allocated and must be freed by the caller.
+ */
 const char* roman_math_add(const char* a, const char* b);
 
 #endif
