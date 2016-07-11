cde511476a49d5c276e98d1e7f2a824b22548350
ryan-boder <ryan.boder@gmail.com>
2016-07-10 22:55:44 -0400

    Added test for empty inputs

--- test/check_add.c
+++ test/check_add.c
@@ -14,11 +14,21 @@ START_TEST (should_return_null_when_given_null_inputs)
 END_TEST
 
 
+START_TEST (should_return_null_when_given_empty_inputs)
+{
+  ck_assert_ptr_eq(NULL, roman_math_add("", ""));
+  ck_assert_ptr_eq(NULL, roman_math_add("I", ""));
+  ck_assert_ptr_eq(NULL, roman_math_add("", "I"));
+}
+END_TEST
+
+
 Suite* check_add_suite(void)
 {
   Suite* suite = suite_create("Add Roman Numerals");
   TCase* core = tcase_create("Core");
   tcase_add_test(core, should_return_null_when_given_null_inputs);
+  tcase_add_test(core, should_return_null_when_given_empty_inputs);
   suite_add_tcase(suite, core);  
   return suite;
 }
