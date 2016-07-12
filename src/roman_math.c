3dc3a13e323a2d76dca7045c6a097871e45c8f7b
ryan-boder <ryan.boder@gmail.com>
2016-07-11 21:43:08 -0400

    Refactored add and subtract into common function

--- src/roman_math.c
+++ src/roman_math.c
@@ -5,27 +5,34 @@
 #include "to_roman.h"
 
 
-const char* roman_math_add(const char* a, const char* b)
+typedef int (*operator)(int,int);
+
+
+int operator_add(int a, int b) { return a + b; }
+int operator_subtract(int a, int b) { return a - b; }
+
+
+const char* roman_math_operation(const char* a, const char* b, operator op)
 {
   if (!is_valid_roman_numeral(a) || !is_valid_roman_numeral(b))
     return NULL;
 
   int lhs = roman_numeral_to_integer(a);
   int rhs = roman_numeral_to_integer(b);
-  int result = lhs + rhs;
+  int result = op(lhs, rhs);
 
   return integer_to_roman_numeral(result);
 }
 
 
+
+const char* roman_math_add(const char* a, const char* b)
+{
+  return roman_math_operation(a, b, operator_add);
+}
+
+
 const char* roman_math_subtract(const char* a, const char* b)
 {
-  if (!is_valid_roman_numeral(a) || !is_valid_roman_numeral(b))
-    return NULL;
-
-  int lhs = roman_numeral_to_integer(a);
-  int rhs = roman_numeral_to_integer(b);
-  int result = lhs - rhs;
-
-  return integer_to_roman_numeral(result);
+  return roman_math_operation(a, b, operator_subtract);
 }
