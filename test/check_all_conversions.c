7cd07ae00249d18cf6ca43630c3bc635e517cd67
ryan-boder <ryan.boder@gmail.com>
2016-07-11 23:08:55 -0400

    Added exaustive integer to roman conversion test

--- test/check_all_conversions.c
+++ test/check_all_conversions.c
@@ -7,7 +7,22 @@
 #include "to_roman.h"
 
 
-START_TEST (should_convert_roman_numerals_to_integers)
+typedef void (*test_function)(const char*,const char*);
+
+
+void test_from_roman(const char* integer, const char* roman)
+{
+  ck_assert_int_eq(atoi(integer), roman_numeral_to_integer(roman));
+}
+
+
+void test_to_roman(const char* integer, const char* roman)
+{
+  ck_assert_str_eq(roman, integer_to_roman_numeral(atoi(integer)));
+}
+
+
+void for_each_roman_numeral(test_function fn)
 {
   FILE* fp = fopen("data/roman_numerals.txt", "r");
   if (NULL == fp)
@@ -20,13 +35,26 @@ START_TEST (should_convert_roman_numerals_to_integers)
     char* integer = strtok(line, "=");
     char* roman = strtok(NULL, "\n");
 
-    ck_assert_int_eq(atoi(integer), roman_numeral_to_integer(roman));
+    fn(integer, roman);
 
     free(line);
     line = NULL;
   }
 
   fclose(fp);
+}
+
+
+START_TEST (should_convert_roman_numerals_to_integers)
+{
+  for_each_roman_numeral(test_from_roman);
+}
+END_TEST
+
+
+START_TEST (should_convert_integers_to_roman_numerals)
+{
+  for_each_roman_numeral(test_to_roman);
 }
 END_TEST
 
@@ -36,6 +64,7 @@ Suite* check_all_conversions_suite(void)
   Suite* suite = suite_create("Exaustive Conversions Test");
   TCase* core = tcase_create("Core");
   tcase_add_test(core, should_convert_roman_numerals_to_integers);
+  tcase_add_test(core, should_convert_integers_to_roman_numerals);
   suite_add_tcase(suite, core);
   return suite;
 }
