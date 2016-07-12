0a4b8966e635c258d58a1538d4cee5f958f18611
ryan-boder <ryan.boder@gmail.com>
2016-07-11 20:05:20 -0400

    Copied remaining validate input tests from add to subtract

--- test/check_subtract.c
+++ test/check_subtract.c
@@ -14,11 +14,30 @@ START_TEST (should_return_NULL_when_given_NULL_input)
 END_TEST
 
 
+START_TEST (should_return_NULL_when_given_empty_input)
+{
+  ck_assert_ptr_eq(NULL, roman_math_add("", ""));
+  ck_assert_ptr_eq(NULL, roman_math_add("I", ""));
+  ck_assert_ptr_eq(NULL, roman_math_add("", "I"));
+}
+END_TEST
+
+
+START_TEST (should_return_NULL_when_given_input_with_invalid_digit)
+{
+  ck_assert_ptr_eq(NULL, roman_math_add("A", "I"));
+  ck_assert_ptr_eq(NULL, roman_math_add("I", "A"));
+}
+END_TEST
+
+
 Suite* check_subtract_suite(void)
 {
   Suite* suite = suite_create("Subtract Roman Numerals");
   TCase* core = tcase_create("Core");
   tcase_add_test(core, should_return_NULL_when_given_NULL_input);
+  tcase_add_test(core, should_return_NULL_when_given_empty_input);
+  tcase_add_test(core, should_return_NULL_when_given_input_with_invalid_digit);
   suite_add_tcase(suite, core);  
   return suite;
 }
