dd6bc5eb7ecae32230ad0f7c9f00bc278d42dac4
ryan-boder <ryan.boder@gmail.com>
2016-07-11 20:16:42 -0400

    Added test NULL when result is 0

--- test/check_subtract.c
+++ test/check_subtract.c
@@ -52,6 +52,15 @@ START_TEST (should_return_MMLXVII_when_given_MMMCCLVI_and_MCLXXXIX)
 END_TEST
 
 
+START_TEST (should_return_NULL_when_result_is_0)
+{
+  ck_assert(NULL == roman_math_subtract("I", "I"));
+  ck_assert(NULL == roman_math_subtract("V", "V"));
+  ck_assert(NULL == roman_math_subtract("XX", "XX"));
+}
+END_TEST
+
+
 Suite* check_subtract_suite(void)
 {
   Suite* suite = suite_create("Subtract Roman Numerals");
@@ -62,6 +71,7 @@ Suite* check_subtract_suite(void)
   tcase_add_test(core, should_return_I_when_given_II_and_I);
   tcase_add_test(core, should_return_II_when_given_IX_and_VII);
   tcase_add_test(core, should_return_MMLXVII_when_given_MMMCCLVI_and_MCLXXXIX);
+  tcase_add_test(core, should_return_NULL_when_result_is_0);
   suite_add_tcase(suite, core);  
   return suite;
 }
